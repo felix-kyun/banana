80001304d517bf0d0c1892b3cb1e7a97752b2fd3
Praise Jacob <iampraisejacob@gmail.com>
2026-03-23 23:38:44 +0530

    refactor: add socket support for client

--- client.c
+++ client.c
@@ -1,11 +1,110 @@
-#include "./shared.h"
+/*
+    Author:   Praise Jacob <iampraisejacob@gmail.com>
+    Repo:     https://github.com/felix-kyun/shl
+    SPDX-License-Identifier: MIT
+    Copyright (c) 2026 Praise Jacob
+ */
+
+#include "client.h"
+#include "shared.h"
+#include <errno.h>
 #include <fcntl.h>
 #include <linux/input.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <termios.h>
 #include <unistd.h>
+
+int
+main(int argc, char** argv)
+{
+    if (argc < 3) {
+        show_usage(argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    char* ip     = argv[1];
+    char* device = argv[2];
+
+    int                fd          = open_device(device);
+    struct sockaddr_in server_addr = create_server_address(ip, PORT);
+    header_t           header      = create_device_header(fd);
+
+    printf("Device Info\n");
+    printf("Name: %s\n", header.name);
+    printf("ID: %x:%x\n", header.id.vendor, header.id.product);
+
+    printf("Connecting to server %s\n", ip);
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        close(fd);
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+    if (setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, &(int) { 1 }, sizeof(int)) < 0) {
+        perror("setsockopt");
+        close(sock);
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+    if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        close(sock);
+        close(fd);
+        perror("connect");
+        exit(EXIT_FAILURE);
+    }
+    puts("Connected");
+
+    // send header
+    if (write_all(sock, &header, sizeof(header)) < 0) {
+        fputs("Failed to send header\n", stderr);
+        close(sock);
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // try to gain exclusive read on the device
+    if (ioctl(fd, EVIOCGRAB, 1) == -1) {
+        fprintf(stderr, "Failed to grab device %s\n", device);
+        perror("ioctl");
+        close(sock);
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Device %s is now exclusive\n", device);
+    printf("Press q to release\n");
+
+    atexit(disable_raw_mode);
+    enable_raw_mode();
+
+    char               c;
+    struct input_event ev;
+    while (1) {
+        if (read(STDIN_FILENO, &c, 1) == 1 && c == 'q') {
+            printf("Releasing exclusive mode\n");
+            ioctl(fd, EVIOCGRAB, 0);
+            break;
+        }
+        ssize_t n = read(fd, &ev, sizeof(ev));
+        if (n == sizeof(ev)) {
+            printf("Event: type=%u, code=%u, value=%u\n", ev.type, ev.code, ev.value);
+            if (write_all(sock, &ev, sizeof(ev)) == -1) {
+                fputs("Failed to send event\n", stderr);
+                break;
+            }
+        }
+    }
+
+    close(sock);
+    close(fd);
+    return 0;
+}
 
 struct termios original_t;
 void
@@ -14,7 +113,7 @@ enable_raw_mode()
     struct termios t;
     tcgetattr(STDIN_FILENO, &original_t);
     t = original_t;
-    t.c_lflag &= ~(ICANON | ECHO);
+    t.c_lflag &= (tcflag_t) ~(ICANON | ECHO);
     t.c_cc[VMIN]  = 0;
     t.c_cc[VTIME] = 0;
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &t);
@@ -27,24 +126,17 @@ disable_raw_mode()
 }
 
 void
-show_usage(char* program_name)
+show_usage(char* name)
 {
-    fprintf(stderr, "Usage: %s <ip> <device>\n", program_name);
+    fprintf(stderr, "Usage: %s <ip> <device>\n", name);
     fputs("    ip: IP address of the server\n", stderr);
     fputs("    device: Path to the input device\n", stderr);
     exit(EXIT_FAILURE);
 }
 
 int
-main(int argc, char** argv)
+open_device(char* device)
 {
-    if (argc < 3) {
-        show_usage(argv[0]);
-        exit(EXIT_FAILURE);
-    }
-
-    char* ip     = argv[1];
-    char* device = argv[2];
 
     // check if the iodev exists
     if (access(device, F_OK) == -1) {
@@ -65,6 +157,27 @@ main(int argc, char** argv)
         exit(EXIT_FAILURE);
     }
 
+    return fd;
+}
+
+struct sockaddr_in
+create_server_address(char* ip, uint16_t port)
+{
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port   = htons(port);
+
+    if (inet_pton(AF_INET, ip, &addr.sin_addr) <= 0) {
+        perror("inet_pton");
+        exit(EXIT_FAILURE);
+    }
+
+    return addr;
+}
+
+header_t
+create_device_header(int fd)
+{
     // fill header for the device
     header_t header = { };
     ioctl(fd, EVIOCGNAME(sizeof header.name), header.name);
@@ -80,39 +193,28 @@ main(int argc, char** argv)
         }
     }
 
-    printf("Device Info\n");
-    printf("Name: %s\n", header.name);
-    printf("ID: 0x%x:0x%x\n", header.id.vendor, header.id.product);
+    return header;
+}
 
-    // try to gain exclusive read on the device
-    if (ioctl(fd, EVIOCGRAB, 1) == -1) {
-        fprintf(stderr, "Failed to grab device %s\n", device);
-        perror("ioctl");
-        close(fd);
-        exit(EXIT_FAILURE);
-    }
+ssize_t
+write_all(int fd, const void* buf, size_t len)
+{
+    const char* ptr       = buf;
+    size_t      remaining = len;
 
-    printf("Device %s is now exclusive\n", device);
-    printf("Press q to release\n");
+    while (remaining > 0) {
+        ssize_t n = send(fd, ptr, remaining, MSG_NOSIGNAL);
 
-    atexit(disable_raw_mode);
-    enable_raw_mode();
-
-    char               c;
-    struct input_event ev;
-    while (1) {
-        if (read(STDIN_FILENO, &c, 1)) {
-            if (c == 'q') {
-                printf("Releasing exclusive mode\n");
-                ioctl(fd, EVIOCGRAB, 0);
-                break;
+        if (n < 0) {
+            if (errno == EINTR) {
+                continue;
             }
+            return -1;
         }
-        if (read(fd, &ev, sizeof(ev))) {
-            printf("Event: type=%u, code=%u, value=%u\n", ev.type, ev.code, ev.value);
-        }
+
+        remaining -= (size_t)n;
+        ptr += n;
     }
 
-    close(fd);
-    return 0;
+    return (ssize_t)len;
 }
---
+++ client.h
@@ -0,0 +1,13 @@
+#include <arpa/inet.h>
+#include <stddef.h>
+#include "shared.h"
+
+int open_device(char* device);
+struct sockaddr_in create_server_address(char* ip, uint16_t port);
+header_t create_device_header(int fd);
+
+ssize_t write_all(int fd, const void* buf, size_t len);
+
+void enable_raw_mode();
+void disable_raw_mode();
+void show_usage(char* name);
